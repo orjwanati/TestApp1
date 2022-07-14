0bb0f8d95805a40828a2c60208e1ea9b838cff29
Orjuwan Alwadeai <orjwanati@hotmail.com>
2022-07-14 10:47:44 -0600

    show drop down only when not logged in

--- Data/MyContext.cs
+++ Data/MyContext.cs
@@ -4,17 +4,7 @@
 
 namespace TestApp1.Data
 {
-    public class MyContext : DbContext
-    {
-        private static readonly string[] Tenants = new[]
-   {
-        "TEN001",
-        "TEN002",
-        "TEN003",
-        "TEN004",
-        "TEN005",
-
-    };
+    
         //public virtual DbSet<Tenant> Tenants { get; set; }
-    }
+    
 }
